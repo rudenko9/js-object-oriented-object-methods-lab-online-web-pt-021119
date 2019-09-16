4438c496e1e416b35f3cfd3e6d7b766fc13d621a
Nataliya R <n.rudenko@gmail.com>
2019-09-16 21:13:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function BoardMember( name, homeState, training) {
   };
   
   this.doCharity = function() {
-    console.log(I like to help people.);
+    console.log( 'I like to help people.');
   }
 }
